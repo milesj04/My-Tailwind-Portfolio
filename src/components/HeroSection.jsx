2b1f888a7937618c558cfce1ecde8d511ea68bdb
Miles Jennings <milesmjennings@gmail.com>
2025-11-29 22:27:26 -0800

    update hero and about section styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,11 @@
 import { ArrowDown } from "lucide-react";
-import Headshot from "@/assets/Headshots/MilesHeadShot-Hyper.jpg";
-import HyperTeamPhoto from "@/assets/Headshots/HX11TeamPhotoFinal.jpg";
-import InternGroupPhoto from "@/assets/Headshots/intern group pic.jfif";
+
 export const HeroSection = () => {
     return ( 
-    <section id="hero" className="relative min-h-screen flex flex-col justify-start pt-32 px-4">  
+    <section id="hero" className="relative min-h-screen flex flex-col justify-center px-4">  
         <div className="container max-w-4xl mx-auto text-center z-10 pt-24">
             <div className="space-y-6">
-                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tight">
                     <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
                     <span className="text-primary opacity-0 animate-fade-in-delay-1"> Miles</span>
                     <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Jennings</span>
@@ -24,15 +22,10 @@ export const HeroSection = () => {
                 </div>
             </div>
         </div>
-        {/* Fading images */}
-        <div className="absolute bottom-40 left-1/2 w-[90%] max-w-3xl h-[400px] -translate-x-1/2">
-            <img src={Headshot} className="fade-img fade-delay-0" />
-            <img src={HyperTeamPhoto} className="fade-img fade-delay-4" />
-            <img src={InternGroupPhoto} className="fade-img fade-delay-8" />
-        </div>
+
         <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce ">
             <span className="text-sm text-muted-foreground mb-2">Scroll</span>
-            <ArrowDown className="h-5 w-5 text-primary" />
+            <ArrowDown className="h-7 w-7 text-primary" />
 
         </div>
     </section>
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -29,7 +29,7 @@ export const ThemeToggle = () => {
     };
 
     return ( 
-        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-3 right-5 z-50 p-2 rounded-full transition-colors duration-300",
          "focus:outlin-hidden"
          )}
          >   
